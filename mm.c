fd68429e47b24952409ae21822c005e80c96244f
XintongLi52 <72223756+XintongLi52@users.noreply.github.com>
2021-07-09 04:14:58 +0800

    Update mm.c

--- mm.c
+++ mm.c
@@ -60,11 +60,11 @@ static size_t align(size_t x)
 
 /* Static global variable and static functions*/
 int WSIZE = 4;       //size for a single word
-int DSIZ = 8;
+int DSIZE = 8;
 int CHUNK = 1 << 8;     //Extend heap by this amount(bytes)
 
 /* assign value to *p */
-static void PUT ((unsigned char *) p, unsigned int value)
+static void PUT (unsigned char * p, unsigned int val)
 {
     *(unsigned int *)(p) = (val);
 
@@ -103,12 +103,12 @@ static unsigned char * HDRP(unsigned char *bp)
 /*Compute the address of block's pointer bp's footer*/
 static unsigned char * FTRP(unsigned char *bp)
 {
-    bp = bp + GET_SIZE(HDRP(bp)) - DSIZE);
+    bp = bp + GET_SIZE(HDRP(bp)) - DSIZE;
     return bp;
 }
 
 /*compute address of next blocks*/
-static unsigned char * NEXT_BLKP(unsigned char *bp)
+static char * NEXT_BLKP(char *bp)
 {
     bp = (char *)(bp) + GET_SIZE(((char *)(bp) - WSIZE));
     return bp;
@@ -124,23 +124,23 @@ static unsigned char * PREV_BLKP(unsigned char *bp)
 
 /*Given the number of words and extend the heap with a new free block*/
 static void *extend_heap(size_t words)
-2 {
-3   char *bp;
-4   size_t size;
-5
-6   /* Allocate an even number of words to maintain alignment */
-7   size = (words % 2) ? (words+1) * WSIZE : words * WSIZE;
-8   if ((long)(bp = mem_sbrk(size)) == -1)
-9   return NULL;
-10
-11  /* Initialize free block header/footer and the epilogue header */
-12  PUT(HDRP(bp), PACK(size, 0)); /* Free block header */
-13  PUT(FTRP(bp), PACK(size, 0)); /* Free block footer */
-14  PUT(HDRP(NEXT_BLKP(bp)), PACK(0, 1)); /* New epilogue header */
-15
-16  return bp;
-    //return coalesce(bp);
-18 }
+{
+   char *bp;
+   size_t size;
+
+   /* Allocate an even number of words to maintain alignment */
+   size = (words % 2) ? (words+1) * WSIZE : words * WSIZE;
+   if ((long)(bp = mem_sbrk(size)) == -1)
+   return NULL;
+
+  /* Initialize free block header/footer and the epilogue header */
+  PUT(HDRP(bp), PACK(size, 0)); /* Free block header */
+  PUT(FTRP(bp), PACK(size, 0)); /* Free block footer */
+  PUT(HDRP(NEXT_BLKP(bp)), PACK(0, 1)); /* New epilogue header */
+
+  return bp;
+//return coalesce(bp);
+}
 
 static void *coalesce(void *bp) 
 {
@@ -186,9 +186,9 @@ bool mm_init(void)
     PUT(HeapList+(1*WSIZE), PACK(DSIZE,1));     //Prologue header
     PUT(HeapList+(2*WSIZE), PACK(DSIZE,1));     //Prologue footer
     PUT(HeapList+(3*WSIZE), PACK(0,1));             //Epilogue header
-    HeapList += (2*;WSIZE);
+    HeapList += (2*WSIZE);
 
-    if (extend_heap(CHUNKSIZE/WSIZE) == NULL) return false;
+    if (extend_heap(CHUNK/WSIZE) == NULL) return false;
     return true;
 }
 
@@ -207,10 +207,10 @@ void* malloc(size_t size)
 void free(void* ptr)
 {
     /* IMPLEMENT THIS */
-    size_t size = GET_SIZE(HDRP(bp));
-4   PUT(HDRP(bp), PACK(size, 0));
-6   PUT(FTRP(bp), PACK(size, 0));
-7   //coalesce(bp);
+    size_t size = GET_SIZE(HDRP(ptr));
+    PUT(HDRP(ptr), PACK(size, 0));
+    PUT(FTRP(ptr), PACK(size, 0));
+   //coalesce(bp);
     return;
 }
 
@@ -220,7 +220,36 @@ void free(void* ptr)
 void* realloc(void* oldptr, size_t size)
 {
     /* IMPLEMENT THIS */
-    
+    size_t oldsize;
+    void *newptr;
+
+    /* If size == 0 then this is just free, and we return NULL. */
+    if(size == 0) {
+	mm_free(oldptr);
+	return 0;
+    }
+
+    /* If oldptr is NULL, then this is just malloc. */
+    if(oldptr == NULL) {
+	return mm_malloc(size);
+    }
+
+    newptr = mm_malloc(size);
+
+    /* If realloc() fails the original block is left untouched  */
+    if(!newptr) {
+	return 0;
+    }
+
+    /* Copy the old data. */
+    oldsize = GET_SIZE(HDRP(oldptr));
+    if(size < oldsize) oldsize = size;
+    memcpy(newptr, oldptr, oldsize);
+
+    /* Free the old block. */
+    mm_free(oldptr);
+
+    return newptr;
     return NULL;
 }
 
